8c2e0a6c43fecd370479af7db5441df55126749c
Sandra <sandraromanrivera@hotmail.com>
2020-02-05 23:17:20 -0600

    Added Notaria model and basic create ticket view

---
+++ tickets-infonot-ios/tickets-infonot-ios/Models/Notaria.swift
@@ -0,0 +1,22 @@
+//
+//  Notaria.swift
+//  tickets-infonot-ios
+//
+//  Created by Sandra Roman on 05/02/20.
+//  Copyright © 2020 sroman. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+class Notaria: ObservableObject {
+    var id: Int = 0
+    var numero: Int = 0
+    var telefono: String = ""
+    var idMunicipio: Int = 0
+    var idDireccion: Int = 0
+    var notario: String = ""
+    var created_at: Date = Date()
+    var updated_at: Date = Date()
+}
