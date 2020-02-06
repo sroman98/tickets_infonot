84acd9b1a5b7a96ef0625ec5751692c6adad5ace
Sandra <sandraromanrivera@hotmail.com>
2020-02-05 23:36:36 -0600

    Added usuario model

--- tickets-infonot-ios/tickets-infonot-ios/Views/ContentView.swift
+++ tickets-infonot-ios/tickets-infonot-ios/Views/ContentView.swift
@@ -9,39 +9,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var name: String = "";
-    @State private var email: String = "";
-    @State private var phone: String = "";
-    @State private var subject: String = "";
-    @State private var description: String = "";
-    
     var body: some View {
-        NavigationView {
-            Form {
-                Picker(selection: .constant(0), label: Text("Notaría")) {
-                    Text("Not 1 Qro").tag(1)
-                    Text("Not 2 Qro").tag(2)
-                }
-
-                TextField("Nombre", text: $name)
-                TextField("Correo", text: $email)
-                TextField("Teléfono", text: $phone)
-                TextField("Asunto", text: $subject)
-                
-                Picker(selection: .constant(0), label: Text("Departamento")) {
-                    Text("Soporte técnico").tag(1)
-                    Text("Ventas").tag(2)
-                }
-                
-                TextField("Descripción", text: $description)
-                
-                Button(action: {print("tapped button")}) {
-                    Text("Subir archivo")
-                }
-            }
-            .navigationBarTitle(Text("Nuevo Ticket"))
-            
-        }
+        Text("Hello world")
     }
 }
 
---
+++ tickets-infonot-ios/tickets-infonot-ios/Views/CreateTicketView.swift
@@ -0,0 +1,54 @@
+//
+//  CreateTicketView.swift
+//  tickets-infonot-ios
+//
+//  Created by Sandra Roman on 05/02/20.
+//  Copyright © 2020 sroman. All rights reserved.
+//
+
+import SwiftUI
+
+struct CreateTicketView: View {
+    private var notarias = [Notaria]()
+    
+    @State private var name: String = "";
+    @State private var email: String = "";
+    @State private var phone: String = "";
+    @State private var subject: String = "";
+    @State private var description: String = "";
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Picker(selection: .constant(0), label: Text("Notaría")) {
+                    Text("Not 1 Qro").tag(1)
+                    Text("Not 2 Qro").tag(2)
+                }
+
+                TextField("Nombre", text: $name)
+                TextField("Correo", text: $email)
+                TextField("Teléfono", text: $phone)
+                TextField("Asunto", text: $subject)
+                
+                Picker(selection: .constant(0), label: Text("Departamento")) {
+                    Text("Soporte técnico").tag(1)
+                    Text("Ventas").tag(2)
+                }
+                
+                TextField("Descripción", text: $description)
+                
+                Button(action: {print("tapped button")}) {
+                    Text("Subir archivo")
+                }
+            }
+            .navigationBarTitle(Text("Nuevo Ticket"))
+            
+        }
+    }
+}
+
+struct CreateTicketView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateTicketView()
+    }
+}
