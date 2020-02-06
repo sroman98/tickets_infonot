e59a58b30a69487843cd11b39e733e1506d48701
Sandra <sandraromanrivera@hotmail.com>
2020-02-05 22:11:24 -0600

    Created blank xcode project for ios app

---
+++ tickets-infonot-ios/tickets-infonot-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tickets-infonot-ios
+//
+//  Created by Sandra Roman on 05/02/20.
+//  Copyright © 2020 sroman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
